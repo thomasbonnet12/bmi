18ffe9e93b2541220b2b53712eb59f5472b5d524
Thomas Bonnet <thomas.bonnet.12@gmail.com>
2020-11-04 22:03:58 +0100

    textTheme added

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class BMICalculator extends StatelessWidget {
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
         accentColor: Colors.purple,
-        textTheme: TextTheme(),
+        textTheme: TextTheme(bodyText2: TextStyle(color: Colors.white)),
       ),
       home: InputPage(),
     );
