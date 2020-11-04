9d116f4045035f533566f62f90f8ab5426eae138
Thomas Bonnet <thomas.bonnet.12@gmail.com>
2020-11-04 22:02:14 +0100

    themedata added with primary ad scaffoldbackground color

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,12 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        accentColor: Colors.purple,
+        textTheme: TextTheme(),
+      ),
       home: InputPage(),
     );
   }
