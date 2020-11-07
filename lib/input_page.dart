9c31e15db69a702137f707dba78092d83139e2f2
Thomas Bonnet <thomas.bonnet.12@gmail.com>
2020-11-07 11:59:15 +0100

    bottom container created

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -51,6 +51,12 @@ class _InputPageState extends State<InputPage> {
               ],
             ),
           ),
+          Container(
+            color: Color(0xFFEB1555),
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: 80.0,
+          ),
         ],
       ),
     );
