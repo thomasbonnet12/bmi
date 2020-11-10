5400fa49292fb16aee49318aaa7fa234edcc3664
Thomas Bonnet <thomas.bonnet.12@gmail.com>
2020-11-10 22:55:43 +0100

    getinterpretation and getresult created in calculatorbrain.dart

--- lib/components/calculator_brain.dart
+++ lib/components/calculator_brain.dart
@@ -1,8 +1,35 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
 class CalculatorBrain {
   CalculatorBrain({this.weight, this.height});
 
   final int height;
   final int weight;
+
+  double _bmi;
+
+  String calculateBMI() {
+    _bmi = weight / pow(height / 100, 2);
+    return _bmi.toStringAsFixed(1);
+  }
+
+  String getResult() {
+    if (_bmi >= 25) {
+      return 'Overweight';
+    } else if (_bmi > 18.5) {
+      return 'Normal';
+    } else {
+      return 'Underweight';
+    }
+  }
+
+  String getInterpretation() {
+    if (_bmi >= 25) {
+      return 'Try to exercice more and eat healthier food';
+    } else if (_bmi > 18.5) {
+      return 'You are fit. If you want to be in better shape you can exercice more';
+    } else {
+      return 'You can eat more if you want !';
+    }
+  }
 }
